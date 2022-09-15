c7779e668487a95e3353984600960c534e0a1844
Chisanan232 <chi10211201@cycu.org.tw>
2022-09-15 09:43:20 +0800

    [New Feature] (code) Add document content at 'BaseCrawler' as KDoc format.

--- lib/smoothcrawler/main/kotlin/org/smoothcrawler/crawler/BaseCrawler.kt
+++ lib/smoothcrawler/main/kotlin/org/smoothcrawler/crawler/BaseCrawler.kt
@@ -2,24 +2,50 @@ package org.smoothcrawler.crawler
 
 import org.smoothcrawler.factory.BaseFactory
 
-abstract class BaseCrawler(private val factory: BaseFactory) {
+/**
+ * Define some basic attributes of web spider.
+ * It also provides some template methods to let subclass extends it or overrides it.
+ *
+ * @property factory the data class which saves some factories for generating components instance web spider needs.
+ */
+abstract class BaseCrawler(private val factory: BaseFactory? = null) {
 
+    /**
+     * A template method for an entire process of crawling data from website and do some processing on it.
+     * @param url the target URL to crawl.
+     * @return the running result which has been handled.
+     */
     //TODO: Please consider about the generic data type. It should be more clear that what data it could receive and return
     open fun <T> crawl(url: String): T {
         val response: T = sendHttpRequest(url = url)
         return parseHttpResponse(response = response)
     }
 
+    /**
+     * A template method for sending HTTP request to target URL.
+     * @param url the target URL to crawl.
+     * @return the HTTP response which is running result of crawling.
+     */
     open fun <T> sendHttpRequest(url: String): T {
-        return this.factory.httpRequest.request(url = url)
+        return this.factory!!.httpRequest.request(url = url)
     }
 
+    /**
+     * A template method for parsing HTTP response. Just ONLY parse the data from HTML.
+     * @param response the target URL to crawl.
+     * @return the parsed data from HTTP response.
+     */
     open fun <T> parseHttpResponse(response: T): T {
-        return this.factory.httpResponseParser.parseContent(response = response)
+        return this.factory!!.httpResponseParser.parseContent(response = response)
     }
 
+    /**
+     * A template method for run data processing of parsed result from HTTP response.
+     * @param parsedResponse the data which has been parsed from HTTP response.
+     * @return the final data.
+     */
     open fun <T> handleData(parsedResponse: T): T {
-        return this.factory.dataHandler.process(data = parsedResponse)
+        return this.factory!!.dataHandler.process(data = parsedResponse)
     }
 
 }
